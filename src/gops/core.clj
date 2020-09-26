15c842b673404fb8710b5906cbc365b5e83757f2
Vincenzo Chianese <vincenz.chianese@icloud.com>
2020-09-26 09:28:27 -0500

    rename and use threading

--- src/gops/core.clj
+++ src/gops/core.clj
@@ -1,30 +1,31 @@
 (ns gops.core
   (:gen-class))
 
-(def initial-state {:firstPlayer {:deck #{1 2 3 4 5 6 7 8 9 10 11} :score 0}
-                    :secondPlayer {:deck #{1 2 3 4 5 6 7 8 9 10 11} :score 0}
-                    :bountyDeck #{1 2 3 4 5 6 7 8 9 10 11}})
+(def initial-state "Initial game state" {:firstPlayer {:deck #{1 2 3 4 5 6 7 8 9 10 11 12 13} :score 0}
+                                         :secondPlayer {:deck #{1 2 3 4 5 6 7 8 9 10 11 12 13} :score 0}
+                                         :bountyDeck #{1 2 3 4 5 6 7 8 9 10 11 12 13}})
 
 (defn random-card-strategy [deck] (rand-nth (seq deck)))
 (defn highest-card-strategy [deck] (apply max (seq deck)))
+(def draw-card random-card-strategy)
 
-(defn gameStep [currentState] (let [bountyDeck (:bountyDeck currentState)
-                                    first-player-deck (get-in currentState [:firstPlayer :deck])
-                                    second-player-deck (get-in currentState [:secondPlayer :deck])
-                                    first-player-score (get-in currentState [:firstPlayer :score])
-                                    second-player-score (get-in currentState [:secondPlayer :score])]
-                                (if (= 0 (count bountyDeck))
-                                  currentState
-                                  (let [drawn-card (rand-nth (seq bountyDeck))
-                                        first-player-card (random-card-strategy first-player-deck)
-                                        second-player-card (highest-card-strategy second-player-deck)
-                                        new-state {:bountyDeck (disj bountyDeck drawn-card)
-                                                   :firstPlayer {:deck (disj first-player-deck first-player-card) :score first-player-score}
-                                                   :secondPlayer {:deck (disj second-player-deck second-player-card) :score second-player-score}}
-                                        matchWinner (if (> first-player-card second-player-card) :firstPlayer :secondPlayer)]
-                                    (gameStep (update-in new-state [matchWinner :score] inc))))))
+(defn game-step [current-state] (let [bountyDeck (:bountyDeck current-state)
+                                      first-player-deck (get-in current-state [:firstPlayer :deck])
+                                      second-player-deck (get-in current-state [:secondPlayer :deck])]
+                                  (if (= 0 (count bountyDeck))
+                                    current-state
+                                    (let [drawn-card (draw-card bountyDeck)
+                                          first-player-card (random-card-strategy first-player-deck)
+                                          second-player-card (highest-card-strategy second-player-deck)
+                                          matchWinner (if (> first-player-card second-player-card) :firstPlayer :secondPlayer)
+                                          new-state (-> current-state
+                                                        (update-in [matchWinner :score] inc)
+                                                        (update-in [:bountyDeck] #(disj % drawn-card))
+                                                        (update-in [:firstPlayer :deck] #(disj % first-player-card))
+                                                        (update-in [:secondPlayer :deck] #(disj % second-player-card)))]
+                                      (game-step new-state)))))
 
 (defn -main []
-  (let [final-state (gameStep initial-state)]
+  (let [final-state (game-step initial-state)]
     (println final-state)
     (if (> (get-in final-state [:firstPlayer :score]) (get-in final-state [:secondPlayer :score])) (println "player 1 won") (println "player 2 won"))))
